a06e23f812115f52e3b0d10335d77d0f82c3a07e
brenndha-cabral <cabralbrenndha@gmail.com>
2025-06-10 10:36:26 -0300

    feat: create dataMenu.ts

---
+++ src/utils/dataMenu.ts
@@ -0,0 +1,8 @@
+export const dataMenu: Array<{ code: number; description: string }> = [
+    { code: 1, description: "Destilar nova poção" },
+    { code: 2, description: "Listar todos os elixires em estoque" },
+    { code: 3, description: "Buscar poção por código alquímico" },
+    { code: 4, description: "Modificar propriedades da poção" },
+    { code: 5, description: "Descartar poção (efeitos irreversíveis!)" },
+    { code: 6, description: "Fechar caldeirão" }
+];
