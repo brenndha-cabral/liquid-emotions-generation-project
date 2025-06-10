e930244426c7c3bf5c2e7e7fc91386ce5b4a5ff7
brenndha-cabral <cabralbrenndha@gmail.com>
2025-06-10 11:57:40 -0300

    feat: create TimeDistortionPotion class

---
+++ src/models/TimeDistortionPotion.ts
@@ -0,0 +1,42 @@
+import { Potion } from "./Potion";
+
+export class TimeDilationPotion extends Potion {
+    private _timeDilationFactor: number;
+    private _possibleParadoxes: string;
+
+    constructor(
+        id: number,
+        description: string,
+        type: string,
+        quantity: number,
+        flavor: string,
+        principalEffect: string,
+        sideEffects: string,
+        howToUse: string,
+        ingredients: string,
+        price: number,
+        stock: number,
+        timeDilationFactor: number,
+        possibleParadoxes: string,
+    ) {
+        super(id, description, type, quantity, flavor, principalEffect, sideEffects, howToUse, ingredients, price, stock);
+        this._timeDilationFactor = timeDilationFactor;
+        this._possibleParadoxes = possibleParadoxes;
+    }
+
+    public get timeDilationFactor() {
+        return this._timeDilationFactor;
+    }
+
+    public set timeDilationFactor(timeDilationFactor: number) {
+        this._timeDilationFactor = timeDilationFactor;
+    }
+
+    public get possibleParadoxes() {
+        return this._possibleParadoxes;
+    }
+
+    public set possibleParadoxes(possibleParadoxes: string) {
+        this._possibleParadoxes = possibleParadoxes;
+    }
+}
