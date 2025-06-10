2bbdc86bdc8f30fb14e372cb2c503df1122b26c5
brenndha-cabral <cabralbrenndha@gmail.com>
2025-06-10 11:46:16 -0300

    feat: create ChaosPotion class

---
+++ src/models/ChaosPotion.ts
@@ -0,0 +1,42 @@
+import { Potion } from "./Potion";
+
+export class ChaosPotion extends Potion {
+    private _destructionLevel: number;
+    private _effectDuration: number;
+
+    constructor(
+        id: number,
+        description: string,
+        type: string,
+        quantity: number,
+        flavor: string,
+        principalEffect: string,
+        sideEffects: string,
+        howToUse: string,
+        ingredients: string,
+        price: number,
+        stock: number,
+        destructionLevel: number,
+        effectDuration: number,
+    ) {
+        super(id, description, type, quantity, flavor, principalEffect, sideEffects, howToUse, ingredients, price, stock);
+        this._destructionLevel = destructionLevel;
+        this._effectDuration = effectDuration;
+    }
+
+    public get destructionLevel() {
+        return this._destructionLevel;
+    }
+
+    public set destructionLevel(destructionLevel: number) {
+        this._destructionLevel = destructionLevel;
+    }
+
+    public get effectDuration() {
+        return this._effectDuration;
+    }
+
+    public set effectDuration(effectDuration: number) {
+        this._effectDuration = effectDuration;
+    }
+}
